49948e20b97f2c11280a8cff30a2820d6d374aa9
raihan-uddin <blackedition.me@gmail.com>
2021-01-05 21:51:55 +0600

    error solved

--- resources/js/components/admin/AdminMaster.vue
+++ resources/js/components/admin/AdminMaster.vue
@@ -340,7 +340,7 @@
         <div class="header-navbar navbar-expand-sm navbar navbar-horizontal navbar-fixed navbar-light navbar-without-dd-arrow navbar-shadow menu-border"
              role="navigation" >
             <!-- Horizontal menu content-->
-            <div class="navbar-container main-menu-content" data-menu="menu-container">
+            <div class="navbar-container main-menu-content" :data-menu="'menu-container'">
                 <!-- include ../../../includes/mixins-->
                 <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
                     <li class="dropdown nav-item" data-menu="dropdown">
